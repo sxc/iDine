184b40401f15a8b7b25cd401f9f4bae14447724e
sxc <qiuyequ@icloud.com>
2019-12-19 10:52:16 +0800

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Xiaochun Shen on 2019/12/19.
+//  Copyright © 2019 SXC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
